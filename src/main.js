7b9d319459864b4d0e4742ac897f8323aa42b2fe
Kashif Khan <kashifkhan0336@gmail.com>
2020-08-05 19:38:43 +0500

    initial

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-
+import vuetify from './plugins/vuetify';
+import VueSocketIOExt from 'vue-socket.io-extended';
+import io from 'socket.io-client/dist/socket.io.slim.js';
 Vue.config.productionTip = false
-
+const socket = io("http://192.168.2.103:8080")
+Vue.use(VueSocketIOExt, socket);
+require("video.js/dist/video-js.css")
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
