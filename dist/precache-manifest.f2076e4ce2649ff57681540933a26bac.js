aed02386ff7253e63381321e612afbe31a0318c6
Kashif Khan <kashifkhan0336@gmail.com>
2020-08-06 11:00:00 +0100

    prod

---
+++ dist/precache-manifest.f2076e4ce2649ff57681540933a26bac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1c3d3cd94f92ffbfe2",
+    "url": "/css/chunk-vendors.97497736.css"
+  },
+  {
+    "revision": "75f5dbe54cda71d0883e12e484cba8c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "342500f35725279245b9",
+    "url": "/js/app.521a5054.js"
+  },
+  {
+    "revision": "5d1c3d3cd94f92ffbfe2",
+    "url": "/js/chunk-vendors.8232b4ac.js"
+  },
+  {
+    "revision": "7a3abcc2d3206835e57e37d7ebd7242d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
